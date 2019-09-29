98d8ec3dd8acc08c322e73b4d0a3a908a8069507
Tony Delattre <todelattre@gmail.com>
2019-09-29 17:33:22 +0200

    :construction: recvfrom not receiving input

--- Réseaux/TP/TP2/main_server.c
+++ Réseaux/TP/TP2/main_server.c
@@ -16,13 +16,13 @@ Server server_create_udp() {
     Server server = (Server)malloc(sizeof(struct _server));
 
     if((server->fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("Socket creation failed.");
+        perror("Socket creation failed");
         exit(SOCKET_ERROR);
     }
 
     server->server.sin_family = AF_INET;
     server->server.sin_addr.s_addr = INADDR_ANY;
-    server->server.sin_port = htons(PORT);
+    server->server.sin_port = PORT;
 
     return server;
 }
@@ -32,35 +32,34 @@ void server_close_and_free(Server this){
     free(this);
 }
 
-void server_bind(Server server, int port) {
-    if(bind(server->fd, (const struct sockaddr*)&server->server, sizeof(server->server.sin_addr)) < 0) {
-        perror("Binding failed.");
+void server_bind(Server this, int port) {
+    if(bind(this->fd, (const struct sockaddr *)&this->server, sizeof(this->server)) < 0) {
+        perror("Binding failed");
         exit(BIND_ERROR);
     }
 }
 
 ssize_t server_receive_udp(Server this, char* buf, size_t size) {
-    ssize_t n = recvfrom(this->fd, (char*)buf, size, MSG_WAITALL, (struct sockaddr * restrict)&this->client, this->sock_len);
+    ssize_t n = recvfrom(this->fd, (char*)buf, size, MSG_PEEK, (struct sockaddr * restrict)&this->client, &this->sock_len);
     buf[n] = '\0';
     return n;
 }
 
 void server_send_udp(Server this, char* msg) {
-    sendto(this->fd, (char*)msg,strlen(msg), MSG_CONFIRM, &this->server, this->sock_len);
+    sendto(this->fd, (char*)msg,strlen(msg), MSG_CONFIRM, (const struct sockaddr *)&this->server, this->sock_len);
 }
 
 int main(){
     char buffer[MAX];
     char *msg;
 
+    Server server = server_create_udp();
+    server_bind(server, PORT);
     for(;;){
-        Server server = server_create_udp();
 
-        server_bind(server, PORT);
+        // réception d'un message:
+        server_receive_udp(server, buffer, MAX);
 
-        //réception d'un message:
-        ssize_t r_size = server_receive_udp(server, buffer, MAX);
-        
         //si le message est PING alors on retourne au client PONG
         if(strcmp("PING", buffer) == 0){
             msg = "PONG";
@@ -68,7 +67,6 @@ int main(){
             msg = "PAS PONG";
         }
         
-        printf("%s\n",buffer);
         //On envoie le msg
         server_send_udp(server, msg);
         
